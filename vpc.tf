ad4e0203e5f919d96da859efe52a16d905e3853b
Sonisachin0263 <86377303+Sonisachin0263@users.noreply.github.com>
2023-03-05 18:21:04 +0530

    adding test file

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.1.0.0/16"
+}
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.1.1.0/24"
+}
+
+resource "aws_subnet" "my_subnet2" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.1.2.0/24"
+}
+
